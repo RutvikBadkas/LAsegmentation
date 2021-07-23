c44af773123c0bff05fd60c14dbe51c5e0d20ef1
Rutvik Badkas <76641624+RutvikBadkas@users.noreply.github.com>
2021-07-23 16:19:18 +0100

    House Keeping

--- simple_unet_model_3d.py
+++ simple_unet_model_3d.py
@@ -1,12 +1,13 @@
-# https://youtu.be/BNPW1mYbgS4
-# u-net model 
+# Modified U-net CNN model 
+# import all the packages.
 from keras import optimizers
 from keras.models import Model
 from keras import Input
 from keras.layers import Dot, Conv3D, MaxPooling3D, UpSampling3D, concatenate, Conv3DTranspose, BatchNormalization, Dropout, Lambda
 from keras import backend as K
 
-# Dice Coeff: https://towardsdatascience.com/medical-images-segmentation-using-keras-7dc3be5a8524
+# Dice Coefficient Function as a measure of accuracy.
+
 def dice_coef(y_true, y_pred):
     smooth=0.0000001
     y_true_f = K.flatten(y_true)
@@ -14,8 +15,12 @@ def dice_coef(y_true, y_pred):
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+# Dice Coefficient Function as Loss function.
+
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
+
+# Jaccard Index Function
 
 def jacard_coef(y_true, y_pred):
     # y_true = y_true[0, :, :, :, :]
@@ -25,12 +30,20 @@ def jacard_coef(y_true, y_pred):
     intersection = K.sum(y_true_f * y_pred_f)
     return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
 
+# Jaccard Index Function as Loss function.
+
 def jacard_coef_loss(y_true, y_pred):
     return 1-jacard_coef(y_true, y_pred)  # -1 ultiplied as we want to minimize this value as loss function
 
-################################################################
+# I didnt derive Jaccard or dice from each other because I didnt want to merge the uncertainties of one into that of another as I compare both the metrics in my research.
+# Below is the main model funciton. This is called in the training file. Changes in the architecture can be very easily made by just removing layers separated by an empty line.
+# Note: be careful because chances are, you will mismatch the dimensions from one layer to another when you edit the model. So do the calculations first of the stride size and 
+# convolutional kernal sizes to see if the output from the previous layer matches the input into this layer. See my research paper for a proposed model with less layers, use
+# that as a basis to reduce the number of layers.
+# Also: do NOT add more layers, the model is very suceptible to overfitting. Would recommend experimenting with the dataset first. I have added training scripts with aug functions
+# to this repo so try that first. 
+
 def simple_unet_model_3d(IMG_DEPTH, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):
-#Build the model
     
     K.set_image_data_format('channels_last')
     inputs = Input((IMG_DEPTH, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
@@ -86,13 +99,20 @@ def simple_unet_model_3d(IMG_DEPTH, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):
     c9 = Conv3D(16, (3, 3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u9)
     c9 = Dropout(0.1)(c9)
     c9 = Conv3D(16, (3, 3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c9)
-     
+    
+    # This is the 1x1x1 convolution layer that I mentioned in my paper, sigmoid only works because the situation is binary, softMax will be required if you add multiple classes
+        
     outputs = Conv3D(1, (1, 1, 1), activation='sigmoid')(c9)
-     
+    
     model = Model(inputs=[inputs], outputs=[outputs])
+    
+    # I recommend you start with the the Adam optimiser first. I only used SGD because it was very case specific and I needed to fine tune the optimizer parameters. 
+    # if you get a lot of spikes in the model, and you don't want to reduce the learning rate, then change the clipnorm paramter in line 116.
+    
     #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     #model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = 'accuracy')
     #optimizer = optimizers.Adam(learning_rate=1)
+       
     optimizer = optimizers.SGD(lr= 0.01, momentum=0.9, nesterov=True, clipnorm=1.)
     
     model.compile(optimizer = optimizer, loss = [dice_coef_loss], metrics = [dice_coef])
